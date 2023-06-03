a3e11a3d7788d31f938a8f087b80cfc98cf94c09
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-03 20:24:45 +0300

    edited code to show 4 results for the component

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,39 +2,22 @@ import data from './data.json';
 // import css from './Statistics.module.css';
 
 const Statistics = () => {
-  for (let i = 0; i < 4; i += 1) {
-    const label = data[i].label;
-    const percentage = data[i].percentage;
-    console.log(label);
-    console.log(percentage);
+  const limitedData = data.slice(0, 4); // Get the first 4 items
 
-    return (
-        <section className="statistics">
+  return (
+    <section className="statistics">
       <h2 className="title">Upload stats</h2>
-    
+
       <ul className="stat-list">
-        <li className="item">
-          <span className="label">{label}</span>
-          <span className="percentage">{percentage}</span>
-        </li>
-        <li className="item">
-          <span className="label">{label}</span>
-          <span className="percentage">{percentage}</span>
-        </li>
-        <li className="item">
-          <span className="label">{label}</span>
-          <span className="percentage">{percentage}</span>
-        </li>
-        <li className="item">
-          <span className="label">{label}</span>
-          <span className="percentage">{percentage}</span>
-        </li>
+        {limitedData.map((item) => (
+          <li className="item" key={item.id}>
+            <span className="label">{item.label}</span>
+            <span className="percentage">{item.percentage}</span>
+          </li>
+        ))}
       </ul>
     </section>
-  )};
-
-  
-  ;
+  );
 };
 
 export default Statistics;
