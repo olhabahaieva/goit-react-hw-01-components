c278d7691e26e09eff3e2d45c4f7c44716336460
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-03 14:54:30 +0300

    created basic structure for components and profile details

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+import data from './user.json';
+import './Profile.css';
+
+//Profile React component
+export const Profile = () => {
+    const { username, tag, location, avatar, stats } = data;
+    <div class="profile">
+  <div class="description">
+    <img
+      src={avatar}
+      alt={username}
+      class="avatar"
+    />
+    <p class="name">{username}</p>
+    <p class="tag">{tag}</p>
+    <p class="location">{location}</p>
+  </div>
+
+  <ul class="stats">
+    <li>
+      <span class="label">Followers</span>
+      <span class="quantity">{stats.followers}</span>
+    </li>
+    <li>
+      <span class="label">Views</span>
+      <span class="quantity">{stats.views}</span>
+    </li>
+    <li>
+      <span class="label">Likes</span>
+      <span class="quantity">{stats.likes}</span>
+    </li>
+  </ul>
+</div>
+}
