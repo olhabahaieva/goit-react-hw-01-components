0c4489950cd6b7f7359337eaf7b85af72246e09b
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-04 20:51:00 +0300

    profile component: moved json to the app.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,33 +1,30 @@
-import user from './user.json';
 import PropTypes from 'prop-types';
 import css from './Profile.module.css';
 
-//Profile React component
-const Profile = () => {
-  const { username, tag, location, avatar, stats } = user;
+const Profile = props => {
   return (
     <div className={css.profile}>
       <div className={css.description}>
-        <img src={avatar} alt={username} className={css.avatar} />
+        <img src={props.avatar} alt={props.username} className={css.avatar} />
         <div className={css.basicInfo}>
-          <p className={css.name}>{username}</p>
-          <p className={css.tag}>{tag}</p>
-          <p className={css.location}>{location}</p>
+          <p className={css.name}>{props.username}</p>
+          <p className={css.tag}>{props.tag}</p>
+          <p className={css.location}>{props.location}</p>
         </div>
       </div>
 
       <ul className={css.stats}>
         <li className={css.statsElement}>
           <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{stats.followers}</span>
+          <span className={css.quantity}>{props.stats.followers}</span>
         </li>
         <li className={css.statsElement}>
           <span className={css.label}>Views</span>
-          <span className={css.quantity}>{stats.views}</span>
+          <span className={css.quantity}>{props.stats.views}</span>
         </li>
         <li className={css.statsElement}>
           <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{stats.likes}</span>
+          <span className={css.quantity}>{props.stats.likes}</span>
         </li>
       </ul>
     </div>
