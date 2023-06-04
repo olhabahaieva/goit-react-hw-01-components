e7c48f344acac1b406eb558d650771fa9bdfbd7a
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-04 14:00:10 +0300

    added styling for friendList

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,16 +2,23 @@ import data from './friends.json';
 import css from './FriendList.module.css';
 
 const FriendList = () => {
-   return <ul className={css.friendList}>
-   {data.map((friend) => (
-    <li className={css.friendElement} key={friend.id}>
-        <div className={css.isOnline}></div>
-        <img className={css.avatar} src={friend.avatar} alt={friend.name}/>
-        <h2>{friend.name}</h2>
-    </li>
-   ))}
- </ul>
-   
-}
+  return (
+    <ul className={css.friendList}>
+      {data.map(friend => (
+        <li className={css.friendElement} key={friend.id}>
+          <div className={css.isOnline}></div>
+          <div className={css.avatar}>
+            <img
+              className={css.avatarImage}
+              src={friend.avatar}
+              alt={friend.name}
+            />
+          </div>
+          <p className={css.name}>{friend.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default FriendList;
