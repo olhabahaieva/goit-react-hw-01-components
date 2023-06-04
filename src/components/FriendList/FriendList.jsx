28db2ebb22d956fc8d0a2a8dd05429e00d26f5a4
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-04 14:11:13 +0300

    added function for statuses

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -4,19 +4,23 @@ import css from './FriendList.module.css';
 const FriendList = () => {
   return (
     <ul className={css.friendList}>
-      {data.map(friend => (
-        <li className={css.friendElement} key={friend.id}>
-          <div className={css.isOnline}></div>
-          <div className={css.avatar}>
-            <img
-              className={css.avatarImage}
-              src={friend.avatar}
-              alt={friend.name}
-            />
-          </div>
-          <p className={css.name}>{friend.name}</p>
-        </li>
-      ))}
+      {data.map(friend => {
+        let statusClass = friend.isOnline ? css.isOnline : css.isOffline;
+
+        return (
+          <li className={css.friendElement} key={friend.id}>
+            <div className={`${css.status} ${statusClass}`}></div>
+            <div className={css.avatar}>
+              <img
+                className={css.avatarImage}
+                src={friend.avatar}
+                alt={friend.name}
+              />
+            </div>
+            <p className={css.name}>{friend.name}</p>
+          </li>
+        );
+      })}
     </ul>
   );
 };
